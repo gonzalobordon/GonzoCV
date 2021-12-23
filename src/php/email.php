468f8692d818a8693f285e2865b27ec56adf6a20
gonzo <bordongonzaloezequiel@gmail.com>
2021-12-23 19:06:27 -0300

    php

--- src/php/email.php
+++ src/php/email.php
@@ -2,23 +2,20 @@
 
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
-$mensaje = $_POST['mensaje'];
+$message = $_POST['mensaje'];
 
-$headers = "From: $nombre <$email>\r\n";
+$header = "From: $nombre <$email>\r\n";
+$header = "X-Mailer: PHP/" . phpversion() . "\r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
 $message = "Mensaje: " .$_POST['mensaje'];
 $message .= "Este mensaje fue enviado por: " . $nombre . " - " . $email;
 $message .= "Enviado el: " . date('d/m/Y', time());
+
 $to = 'thekingstondj@gmail.com';
 $subject = 'Test email';
 
-//send email
-function send_email($to, $subject, $message, $headers) {
-    if (mail($to, $subject, $message, $headers)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-header("Location: ../index.html");
+mail($to, $subject, utf8_decode($message), $header);
+header("Location:index.html");
 ?>
